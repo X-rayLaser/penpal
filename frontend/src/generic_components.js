49bcc2e3041c7adac9944d38e445ac15ea9e059f
Eugene <supernovaprotocol@gmail.com>
2024-01-17 21:49:47 +0700

    Building reusable component for displaying list of objects and form for a new item, refactor

---
+++ frontend/src/generic_components.js
@@ -0,0 +1,291 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Spinner from 'react-bootstrap/Spinner';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Accordion from 'react-bootstrap/Accordion';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import { withRouter } from "./utils";
+
+
+class ItemListWithForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            fetchingItems: true,
+            itemsFetchError: "",
+
+            submissionInProgress: false,
+            deletionInProgress: false,
+
+            errors: [],
+            submissionError: "",
+            deletionError: "",
+
+            showCreatedToast: false,
+            showDeletedToast: false
+        };
+
+        this.listUrl = '';
+        this.itemsHeader = "Items";
+        this.formHeader = "Create new item";
+        this.itemCreatedText = "You've successfully added a new item";
+        this.itemDeletedText = "You've successfully deleted an item";
+
+        this.invalidFieldsText = "Some fields are not filled correctly";
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDeleteItem = this.handleDeleteItem.bind(this);
+        this.handleHideCreatedToast = this.handleHideCreatedToast.bind(this);
+        this.handleHideDeletedToast = this.handleHideDeletedToast.bind(this);
+    }
+
+    componentDidMount() {
+        let fetcher = new GenericFetchJson();
+        fetcher.messages["404"] = "Failed to fetch items: resource not found (404)";
+        fetcher.messages["5xx"] = "Failed to fetch items because of server error";
+        fetcher.invalidFieldsError = this.invalidFieldsText;
+        fetcher.unknownError = "Failed to fetch items because of unknown error";
+
+        fetcher.performFetch(this.listUrl).then(data => {
+            let items = data.map((it, index) => {
+                let item = this.createItem(it);
+                item.index = index;
+                return item;
+            });
+
+            this.setState({ items });
+        }).catch(errorObject => {
+            this.setState({ itemsFetchError: errorObject.error });
+        }).finally(() => {
+            this.setState({ fetchingItems: false });
+        });
+    }
+
+    createItem(item) {
+        throw "Not implmented";
+    }
+
+    renderItem(item, index, handleDeleteItem) {
+        return (
+            <div key={index}>List item</div>
+        );
+    }
+
+    renderForm(handleSubmit) {
+        throw "Not implmented";
+    }
+
+    handleSubmit(fieldsObject) {
+        this.setState({ submissionInProgress: true });
+
+        let fetcher = new GenericFetchJson();
+        fetcher.messages["404"] = "Submission failed because resource not found (404)";
+        fetcher.messages["5xx"] = "Submission failed because of server error";
+        fetcher.invalidFieldsError = this.invalidFieldsText;
+        fetcher.unknownError = "Submission failed due to an unknown error";
+
+        fetcher.method = "POST";
+        fetcher.body = fieldsObject;
+
+        return fetcher.performFetch(this.listUrl).then(data => {
+            this.setState(prevState => {
+                let newItem = this.createItem(data);
+                newItem.index = `${prevState.items.length}`;
+
+                return {
+                    items: [...prevState.items, newItem],
+                    errors: [],
+                    submissionError: "",
+                    showCreatedToast: true
+                }
+            });
+        }).catch(reason => {
+            console.error(reason);
+
+            let updatedFields = { submissionError: reason.error };
+
+            if (reason.hasOwnProperty("fieldErrors")) {
+                updatedFields.errors = reason.fieldErrors;
+            }
+
+            this.setState(updatedFields);
+        }).finally(() => {
+            this.setState({ submissionInProgress: false });
+        });
+    }
+
+    handleDeleteItem(item) {
+        let detailUrl = `${this.listUrl}${item.id}/`;
+
+        this.setState({ deletionInProgress: true });
+
+        let fetcher = new GenericFetchJson();
+        fetcher.messages["404"] = "Failed to delete item: resource not found (404)";
+        fetcher.messages["5xx"] = "Failed to delete item: server error";
+        fetcher.invalidFieldsError = this.invalidFieldsText;
+        fetcher.unknownError = "Failed to delete item: unknown error";
+        fetcher.method = "DELETE";
+        fetcher.okRespondWithJson = false;
+
+        fetcher.performFetch(detailUrl).then(data => {
+            this.setState(prevState => {
+                let newItems = prevState.items.filter(it => it.id !== item.id);
+                return {
+                    items: newItems,
+                    showDeletedToast: true,
+                    deletionError: ""
+                };
+            });
+        }).catch(reason => {
+            this.setState({ deletionError: reason.error });
+        }).finally(() => {
+            this.setState({  deletionInProgress: false });
+        });
+    }
+
+    handleHideCreatedToast() {
+        this.setState({ showCreatedToast: false });
+    }
+
+    handleHideDeletedToast() {
+        this.setState({ showDeletedToast: false });
+    }
+
+    render() {
+        let listItems = this.state.items.map((item, index) => this.renderItem(item, index, this.handleDeleteItem));
+        let form = this.renderForm(this.handleSubmit);
+
+        return (
+            <div>
+                <StickyToastContainer>
+                    <AutoclosableToast show={this.state.showCreatedToast}
+                                            text={this.itemCreatedText}
+                                            onClose={this.handleHideCreatedToast} />
+
+                    <AutoclosableToast show={this.state.showDeletedToast}
+                                            text={this.itemDeletedText}
+                                            onClose={this.handleHideDeletedToast} />
+                </StickyToastContainer>
+
+                <Card className="mt-2">
+                    <Card.Body>
+                        <Card.Title>{this.formHeader}</Card.Title>
+                        {this.state.submissionError && 
+                            <Alert className="mt-3 mb-3" variant="danger">{this.state.submissionError}</Alert>
+                        }
+                        {form}
+                    </Card.Body>
+                </Card>
+                <h2 className="mt-2 mb-2">{this.itemsHeader}</h2>
+                {this.state.deletionError && <Alert variant="danger">{this.state.deletionError}</Alert>}
+                {this.state.fetchingItems && <Spinner />}
+                {this.state.itemsFetchError && <Alert variant="danger">{this.state.itemsFetchError}</Alert>}
+                {!this.state.fetchingItems && <Accordion>{listItems}</Accordion>}
+            </div>
+        );
+    }
+}
+
+
+class GenericFetchJson {
+    constructor() {
+        this.method = 'GET';
+        this.bodyObject = {};
+        this.okRespondWithJson = true;
+
+        this.messages = {
+            "404": "Fetch failed: resource not found (404)",
+            "5xx": "Fetch failed: server error"
+        };
+
+        this.invalidFieldsError = "Some fields are not filled correctly";
+        this.unknownError = "Fetch failed due to an unknown error";
+        this.unhandledStatusError = this.unknownError;
+    }
+
+    performFetch(url) {
+
+        let requestParams = {
+            method: this.method,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        };
+
+        if (this.method === 'POST') {
+            requestParams.body = JSON.stringify(this.body);
+        }
+
+        let promise = fetch(url, requestParams).then(response => {
+            if (response.ok) {
+                if (this.okRespondWithJson) {
+                    return response.json();
+                } else {
+                    return {};
+                }
+            } else if (response.status === 404) {
+                throw { message: this.messages["404"] };
+            } else if (response.status >= 500) {
+                throw { message: this.messages["5xx"] };
+            } else if (response.status >= 400) {
+                return response.json().then(errorsObject => {
+                    throw { errorsObject };
+                });
+            } else {
+                throw { message: this.unhandledStatusError };
+            }
+        }).catch(reason => {
+            let errorObject;
+            if (reason.hasOwnProperty("errorsObject")) {
+                errorObject = {
+                    fieldErrors: reason.errorsObject,
+                    error: this.invalidFieldsError
+                };
+            } else if (reason.hasOwnProperty("message")) {
+                errorObject = { error: reason.message };
+            } else {
+                errorObject = { error: this.unknownError };
+            }
+
+            throw errorObject;
+        });
+
+        return promise;
+    }
+};
+
+function AutoclosableToast(props) {
+    return (
+        <Toast className="mt-2" bg="success" show={props.show} 
+            delay={3000} autohide onClose={props.onClose}
+            style={{ fontSize: "1.25em" }}>
+            <Toast.Header>
+                <strong className="me-auto">Penpal</strong>
+            </Toast.Header>
+            <Toast.Body className="text-white">{props.text}</Toast.Body>
+        </Toast>
+    );
+}
+
+
+function StickyToastContainer(props) {
+    return (
+        <ToastContainer style={{ position: 'fixed', bottom: 0}}>
+            {props.children}
+        </ToastContainer>
+    );
+}
+
+export {
+    ItemListWithForm,
+    AutoclosableToast,
+    StickyToastContainer
+};
--- frontend/src/presets.js
+++ frontend/src/presets.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
-import Spinner from 'react-bootstrap/Spinner';
-import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Accordion from 'react-bootstrap/Accordion';
-import Toast from 'react-bootstrap/Toast';
-import ToastContainer from 'react-bootstrap/ToastContainer';
+import { ItemListWithForm } from "./generic_components";
 import { withRouter } from "./utils";
 
 
@@ -136,78 +133,14 @@ function Preset(props) {
 }
 
 
-function AutoclosableToast(props) {
-    return (
-        <Toast className="mt-2" bg="success" show={props.show} 
-            delay={3000} autohide onClose={props.onClose}
-            style={{ fontSize: "1.25em" }}>
-            <Toast.Header>
-                <strong className="me-auto">Penpal</strong>
-            </Toast.Header>
-            <Toast.Body className="text-white">{props.text}</Toast.Body>
-        </Toast>
-    );
+function copyObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
 }
 
-
-function StickyToastContainer(props) {
-    return (
-        <ToastContainer style={{ position: 'fixed', bottom: 0}}>
-            {props.children}
-        </ToastContainer>
-    );
-}
-
-
-function NewPresetForm(props) {
-    let nameErrors = [];
-
-    if (props.errors.hasOwnProperty("name")) {
-        nameErrors = props.errors.name.map((error, index) => 
-            <Alert key={index} className="mt-2 mb-2" variant="danger">{error}</Alert>
-        );
-    }
-
-    // todo: turn this into a component, move related event handlers and fetch calls here
-
-    return (
-        <Card className="mt-2">
-            <Card.Body>
-                <Card.Title>Create a new preset</Card.Title>
-                {props.submissionError && 
-                    <Alert className="mt-3 mb-3" variant="danger">{props.submissionError}</Alert>
-                }
-                <Form onSubmit={props.onSubmitForm}>
-                    <Form.Group className="mb-3">
-                        <Form.Label htmlFor="new_preset_name">Name</Form.Label>
-                        <Form.Control id="new_preset_name" name="name" type="text"
-                                        placeholder="Name of your new preset" 
-                                        value={props.presetName}
-                                        onChange={props.onNameChange} />
-
-                        {nameErrors.length > 0 && 
-                            <div>{nameErrors}</div>
-                        }
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <GenerationSettings settings={props.settings} eventHandlers={props.eventHandlers}
-                                            errors={props.errors} />
-                    </Form.Group>
-                    <Button type="submit" disabled={props.submissionInProgress}>
-                        Create preset
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
-    );
-}
-
-
-class PresetsPage extends React.Component {
+class NewPresetForm extends React.Component {
     constructor(props) {
         super(props);
-
-        this.url = "/chats/presets/";
+        
         this.defaultSettings = {
             temperature: 0.3,
             topK: 40,
@@ -218,91 +151,31 @@ class PresetsPage extends React.Component {
         };
 
         this.state = {
-            presets: [],
             name: "",
             settings: this.defaultSettings,
-
-            submissionError: "",
-            errors: {},
-            fetchPresetsError: "",
-            deletionError: "",
-
-            submissionInProgress: false,
-            deletionInProgress: false,
-            fetchPresetsInProgress: true,
-
-            showCreatedPresetToast: false,
-            showDeletedPresetToast: false
+            submissionInProgress: false
         };
 
         this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleSubmitForm = this.handleSubmitForm.bind(this);
-        this.handleDeletePreset = this.handleDeletePreset.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
-        this.handleHideCreatedToast = this.handleHideCreatedToast.bind(this);
-        this.handleHideDeletedToast = this.handleHideDeletedToast.bind(this);
-    }
-
-    componentDidMount() {
-        fetch(this.url, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else if (response.status === 404) {
-                throw { message: "Failed to fetch presets: resource not found (404)" };
-            } else if (response.status >= 500) {
-                throw { message: "Failed to fetch presets because of server error" };
-            } else {
-                throw { message: "Failed to fetch presets because of unknown error" };
-            }
-        }).then(data => {
-            let presets = data.map((fieldObject, index) => {
-                return {
-                    id: fieldObject.id,
-                    name: fieldObject.name,
-                    eventKey: `${index}`,
-                    settings: {
-                        temperature: fieldObject.temperature,
-                        topK: fieldObject.top_k,
-                        topP: fieldObject.top_p,
-                        minP: fieldObject.min_p,
-                        repeatPenalty: fieldObject.repeat_penalty,
-                        nPredict: fieldObject.n_predict
-                    }
-                }
-            })
-            this.setState({ presets });
-        }).catch(reason => {
-            if (reason.hasOwnProperty("message")) {
-                this.setState({ fetchPresetsError: reason.message });
-            } else {
-                this.setState({ fetchPresetsError: "Failed to fetch presets due to an unknown error" });
-            }
-        }).finally(() => {
-            this.setState({ fetchPresetsInProgress: false });
-        })
-    }
-
-    copyObject(obj) {
-        return JSON.parse(JSON.stringify(obj));
-    }
-
-    unpdateSettingsField(settings, field, value) {
-        settings = this.copyObject(settings);
-        settings[field] = value;
-        this.setState({ settings });
     }
 
     handleNameChange(e) {
         this.setState({ name: e.target.value });
     }
 
-    handleSubmitForm(e) {
-        e.preventDefault();
+    unpdateSettingsField(settings, field, value) {
+        settings = copyObject(settings);
+        settings[field] = value;
+        this.setState({ settings });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ submissionInProgress: true });
+
         let body = {
             name: this.state.name,
             temperature: this.state.settings.temperature,
@@ -313,116 +186,9 @@ class PresetsPage extends React.Component {
             n_predict: this.state.settings.nPredict
         };
 
-        this.setState({ submissionInProgress: true });
-
-        fetch(this.url, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(body)
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else if (response.status === 404) {
-                throw { message: "Submission failed because resource not found (404)" };
-            } else if (response.status >= 500) {
-                throw { message: "Submission failed because of server error" };
-            } else if (response.status >= 400) {
-                return response.json().then(errorsObject => {
-                    console.log("HERE")
-                    throw { errorsObject };
-                });
-            }
-        }).then(data => {
-            this.setState(prevState => {
-                let newPreset = {
-                    id: data.id,
-                    name: data.name,
-                    eventKey: `${prevState.presets.length}`,
-                    settings: {
-                        temperature: data.temperature,
-                        topK: data.top_k,
-                        topP: data.top_p,
-                        minP: data.min_p,
-                        repeatPenalty: data.repeat_penalty,
-                        nPredict: data.n_predict
-                    }
-                };
-                return {
-                    presets: [...prevState.presets, newPreset],
-                    errors: [],
-                    submissionError: "",
-                    name: "",
-                    settings: this.copyObject(this.defaultSettings),
-                    showCreatedPresetToast: true
-                }
-            });
-        }).catch(reason => {
-            console.error(reason);
-
-            if (reason.hasOwnProperty("errorsObject")) {
-                this.setState({
-                    errors: reason.errorsObject,
-                    submissionError: "Some fields are not filled correctly"
-                });
-            } else if (reason.hasOwnProperty("message")) {
-                this.setState({ submissionError: reason.message });
-            } else {
-                this.setState({ submissionError: "Submission failed due to an unknown error" });
-            }
-        }).finally(() => {
+        this.props.onSubmit(body).finally(() => {
             this.setState({ submissionInProgress: false });
         });
-    }
-
-    handleDeletePreset(preset) {
-        let detailUrl = `/chats/presets/${preset.id}/`;
-
-        this.setState({ deletionInProgress: true });
-        fetch(detailUrl, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                return {};
-            } else if (response.status === 404) {
-                throw { message: "Failed to delete preset: resource not found (404)" };
-            } else if (response.status >= 500) {
-                throw { message: "Failed to delete preset: server error" };
-            } else {
-                throw { message: "Failed to delete preset: unknown error" };
-            }
-        }).then(data => {
-            this.setState(prevState => {
-                let newPresets = prevState.presets.filter(p => p.id !== preset.id);
-                return {
-                    presets: newPresets,
-                    showDeletedPresetToast: true,
-                    deletionError: ""
-                };
-            });
-        }).catch(reason => {
-            if (reason.hasOwnProperty("message")) {
-                this.setState({ deletionError: reason.message });
-            } else {
-                this.setState({ deletionError: "Failed to delete preset due to an unknown error" });
-            }
-        }).finally(() => {
-            this.setState({  deletionInProgress: false });
-        });
-    }
-
-    handleHideCreatedToast() {
-        this.setState({ showCreatedPresetToast: false });
-    }
-
-    handleHideDeletedToast() {
-        this.setState({ showDeletedPresetToast: false });
     }
 
     render() {
@@ -459,42 +225,75 @@ class PresetsPage extends React.Component {
             onMaxTokensChange
         };
 
-        let accordionItems = this.state.presets.map((p, index) => 
-            <Preset key={index} eventKey={p.eventKey} name={p.name} preset={p} 
-                    onDeletePreset={this.handleDeletePreset}
-                    disableDelete={this.state.deletionInProgress || this.state.submissionInProgress} />
-        );
+        let nameErrors = [];
 
+        if (this.props.errors.hasOwnProperty("name")) {
+            nameErrors = this.props.errors.name.map((error, index) => 
+                <Alert key={index} className="mt-2 mb-2" variant="danger">{error}</Alert>
+            );
+        }
         return (
-            <div>
-                <StickyToastContainer>
-                    <AutoclosableToast show={this.state.showCreatedPresetToast}
-                                            text="You've successfully added a new preset"
-                                            onClose={this.handleHideCreatedToast} />
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor="new_preset_name">Name</Form.Label>
+                    <Form.Control id="new_preset_name" name="name" type="text"
+                                    placeholder="Name of your new preset" 
+                                    value={this.state.name}
+                                    onChange={this.handleNameChange} />
 
-                    <AutoclosableToast show={this.state.showDeletedPresetToast}
-                                            text="You've successfully deleted a preset"
-                                            onClose={this.handleHideDeletedToast} />
-                </StickyToastContainer>
-
-                <NewPresetForm submissionError={this.state.submissionError}
-                               onSubmitForm={this.handleSubmitForm}
-                               presetName={this.state.name}
-                               onNameChange={this.handleNameChange}
-                               settings={this.state.settings}
-                               eventHandlers={handlers}
-                               errors={this.state.errors}
-                               submissionInProgress={this.state.submissionInProgress} />
-
-                <h2 className="mt-2 mb-2">Presets</h2>
-                {this.state.deletionError && <Alert variant="danger">{this.state.deletionError}</Alert>}
-                {this.state.fetchPresetsInProgress && <Spinner />}
-                {this.state.fetchPresetsError && <Alert variant="danger">{this.state.fetchPresetsError}</Alert>}
-                {!this.state.fetchPresetsInProgress && <Accordion>{accordionItems}</Accordion>}
-            </div>
+                    {nameErrors.length > 0 && 
+                        <div>{nameErrors}</div>
+                    }
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <GenerationSettings settings={this.state.settings} eventHandlers={handlers}
+                                        errors={this.props.errors} />
+                </Form.Group>
+                <Button type="submit" disabled={this.props.submissionInProgress}>Submit</Button>
+            </Form>
         );
     }
-};
+}
+
+class PresetsPage extends ItemListWithForm {
+    constructor(props) {
+        super(props);
+
+        this.listUrl = "/chats/presets/";
+        this.itemsHeader = "Presets";
+    }
+
+    createItem(item) {
+        return {
+            id: item.id,
+            name: item.name,
+            settings: {
+                temperature: item.temperature,
+                topK: item.top_k,
+                topP: item.top_p,
+                minP: item.min_p,
+                repeatPenalty: item.repeat_penalty,
+                nPredict: item.n_predict
+            }
+        };
+    }
+
+    renderItem(item, index, handleDeleteItem) {
+        return (
+                <Preset key={index} eventKey={index} name={item.name} preset={item}
+                    onDeletePreset={handleDeleteItem}
+                    disableDelete={this.state.deletionInProgress || this.state.submissionInProgress} />
+        );
+    }
+
+    renderForm(handleSubmit) {
+        return (
+            <NewPresetForm onSubmit={handleSubmit}
+                           errors={this.state.errors}
+                           submissionInProgress={this.state.submissionInProgress} />
+        );
+    }
+}
 
 PresetsPage = withRouter(PresetsPage);
 
